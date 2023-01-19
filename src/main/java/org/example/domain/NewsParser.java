2575e39de54926288aaf188191ce6d613fa8a69f
Evgheni_Pirvan <eparvan@gmail.com>
2023-01-19 17:33:43 +0200

    Refactor code

--- src/main/java/org/example/domain/NewsParser.java
+++ src/main/java/org/example/domain/NewsParser.java
@@ -11,53 +11,56 @@ import java.util.List;
 
 public class NewsParser {
     private NewsData newsData;
-    public NewsParser(NewsDataSource newsDataSource){
+
+    public NewsParser(NewsDataSource newsDataSource) {
         newsData = newsDataSource.readData();
     }
-   public Integer getNumberOfArticles(){
+
+    public Integer getNumberOfArticles() {
         return newsData.totalResults;
     }
 
-    public List<String> printAllArticles(){
-       List<String> titleArticle = new ArrayList<>();
-       for(Article item: newsData.articles){
-           titleArticle.add(item.title);
-       }
-       return titleArticle;
+    public List<String> printAllArticles() {
+        List<String> titleArticle = new ArrayList<>();
+        for (Article item : newsData.articles) {
+            titleArticle.add(item.title);
+        }
+        return titleArticle;
     }
 
 
-    public List<String> printArticleDescriptionById(Integer id){
+    public List<String> printArticleDescriptionById(Integer id) {
         List<String> descriptionArticle = new ArrayList<>();
-        for(Article item: newsData.articles){
+        for (Article item : newsData.articles) {
             descriptionArticle.add(item.description);
         }
         return descriptionArticle;
     }
-    public List<String> printArticleFullDescriptionById(Integer id){
+
+    public List<String> printArticleFullDescriptionById(Integer id) {
         List<String> descriptionArticle = new ArrayList<>();
-        for(Article item: newsData.articles){
+        for (Article item : newsData.articles) {
             descriptionArticle.add(item.fullDescription);
         }
         return descriptionArticle;
     }
 
-    public List<String> searchByKeywords(String key){
+    public List<String> searchByKeywords(String key) {
         List<String> listArticle = new ArrayList<>();
-       for(int i = 0; i<newsData.articles.size(); i++){
-           if(newsData.articles.get(i).keywords !=null) {
-               if (newsData.articles.get(i).keywords.contains(key)) {
-                   listArticle.add(newsData.articles.get(i).title);
-               }
-           }
+        for (int i = 0; i < newsData.articles.size(); i++) {
+            if (newsData.articles.get(i).keywords != null) {
+                if (newsData.articles.get(i).keywords.contains(key)) {
+                    listArticle.add(newsData.articles.get(i).title);
+                }
+            }
         }
-       Collections.sort(listArticle, Collections.reverseOrder());
+        Collections.sort(listArticle, Collections.reverseOrder());
         return listArticle;
     }
 
     public Integer getTotalWordsCount() {
         Integer result = 0;
-        for(Article article : newsData.articles){
+        for (Article article : newsData.articles) {
             result += getWordsCountForArticle(article);
         }
         return result;
@@ -67,18 +70,19 @@ public class NewsParser {
         Article article = newsData.articles.get(articleId);
         return getWordsCountForArticle(article);
     }
-    private Integer getWordsCountForArticle(Article article){
+
+    private Integer getWordsCountForArticle(Article article) {
         Integer result = 0;
-        result+=getsWordsCount(article.title);
-        result+=getsWordsCount(article.description);
-        result+=getsWordsCount(article.fullDescription);
+        result += getsWordsCount(article.title);
+        result += getsWordsCount(article.description);
+        result += getsWordsCount(article.fullDescription);
         return result;
     }
 
-    private Integer getsWordsCount(String text){
-        if(text == null || text.isBlank()){
+    private Integer getsWordsCount(String text) {
+        if (text == null || text.isBlank()) {
             return 0;
-        }else{
+        } else {
             return text.split("\\s*(\\s|,|!|\\.)\\s*").length;
         }
     }
@@ -87,8 +91,8 @@ public class NewsParser {
     public List<Article> searchByTitle(String title) {
         List<Article> listArticles = new ArrayList<>();
         for (Article item : newsData.articles) {
-            if (item.title !=null){
-                if(item.title.contains(title)){
+            if (item.title != null) {
+                if (item.title.contains(title)) {
                     listArticles.add(item);
                 }
             }
@@ -99,8 +103,8 @@ public class NewsParser {
     public List<Article> searchByDescription(String descriptionSearch) {
         List<Article> listArticles = new ArrayList<>();
         for (Article item : newsData.articles) {
-            if (item.description !=null){
-                if(item.description.contains(descriptionSearch)){
+            if (item.description != null) {
+                if (item.description.contains(descriptionSearch)) {
                     listArticles.add(item);
                 }
             }
@@ -111,8 +115,8 @@ public class NewsParser {
     public List<Article> searchByCreator(String creatorSearch) {
         List<Article> listArticles = new ArrayList<>();
         for (Article item : newsData.articles) {
-            if (item.creator !=null){
-                if(item.creator.contains(creatorSearch)){
+            if (item.creator != null) {
+                if (item.creator.contains(creatorSearch)) {
                     listArticles.add(item);
                 }
             }
@@ -122,25 +126,25 @@ public class NewsParser {
 
     public Integer searchWordOccurence(String word, int id) {
         int count = 0;
-        if(id == -1){
-            for(Article article : newsData.articles){
+        if (id == -1) {
+            for (Article article : newsData.articles) {
                 count += countWord(word, article);
             }
-        }else{
+        } else {
             count = countWord(word, newsData.articles.get(id));
         }
         return count;
     }
-    
-    private Integer countWord(String word, Article articles){
-        int count =0;
-        if(articles.title !=null){
-            count += countWordsOccurenses(word,articles.title);
+
+    private Integer countWord(String word, Article articles) {
+        int count = 0;
+        if (articles.title != null) {
+            count += countWordsOccurenses(word, articles.title);
         }
-        if(articles.description !=null){
+        if (articles.description != null) {
             count += countWordsOccurenses(word, articles.description);
         }
-        if(articles.fullDescription !=null){
+        if (articles.fullDescription != null) {
             count += countWordsOccurenses(word, articles.fullDescription);
         }
         return count;
--- src/main/java/org/example/presentation/Main.java
+++ src/main/java/org/example/presentation/Main.java
@@ -14,10 +14,11 @@ public class Main {
     private static Boolean isRunning = true;
     private static NewsDataSource dataSource = new NewsDataSource("src/main/java/org/example/news_data.json");
     private static NewsParser parser = new NewsParser(dataSource);
+
     public static void main(String[] args) {
         setUpMenu(parser);
         menu.printMenu();
-        while (isRunning){
+        while (isRunning) {
             System.out.println("\nAlege optiunea:");
             menu.onMenuItemSelected(console.nextInt());
         }
@@ -25,162 +26,154 @@ public class Main {
 
     private static void setUpMenu(NewsParser parser) {
         menu.addMenuItem("Meniul aplicatiei ", menu::printMenu);
-        menu.addMenuItem("Afisarea numarului total de articole", ()->{
+        menu.addMenuItem("Afisarea numarului total de articole", () -> {
             System.out.println("Numarul de articole: " + parser.getNumberOfArticles());
         });
-        menu.addMenuItem("Afisarea antetelor articolelor",()->{
+        menu.addMenuItem("Afisarea antetelor articolelor", () -> {
             System.out.println("Lista antetelor articolelor: ");
-            for (String item: parser.printAllArticles()) {
+            for (String item : parser.printAllArticles()) {
                 System.out.println(item);
             }
         });
-        menu.addMenuItem("Afisarea antetelor articolelor dupa ID",()->{
+        menu.addMenuItem("Afisarea antetelor articolelor dupa ID", () -> {
             System.out.print("Introdu identificatorul articolului sau apasa enter:");
             String articleId = new Scanner(System.in).nextLine();
-            try{
+            try {
                 Integer id = Integer.parseInt(articleId);
-                List<String> listArticle =parser.printAllArticles();
-                if(id >= 0 && id < listArticle.size()){
+                List<String> listArticle = parser.printAllArticles();
+                if (id >= 0 && id < listArticle.size()) {
                     System.out.println("Antetul cu numarul " + id + " este:\n" + listArticle.get(id));
-                }else{
+                } else {
                     System.out.println("Nu exista astfel de antet!");
                 }
-
-            }catch(NumberFormatException e){
+            } catch (NumberFormatException e) {
                 System.out.println("Nu ati introdus un numar corect!");
             }
-
         });
-        menu.addMenuItem("Afisarea descrierii articolelor dupa ID",()->{
+        menu.addMenuItem("Afisarea descrierii articolelor dupa ID", () -> {
             System.out.print("Introdu identificatorul articolului sau apasa enter:");
             String articleId = new Scanner(System.in).nextLine();
-            try{
+            try {
                 Integer id = Integer.parseInt(articleId);
                 List<String> listArticle = parser.printArticleDescriptionById(id);
-                if(id >= 0 && id < listArticle.size()){
+                if (id >= 0 && id < listArticle.size()) {
                     System.out.println("Descrierea articolului cu numarul " + id + " este:\n" + listArticle.get(id));
-                }else{
+                } else {
                     System.out.println("Nu exista astfel de articol!");
                 }
-            }catch(NumberFormatException e){
+            } catch (NumberFormatException e) {
                 System.out.println("Nu ati introdus un numar corect!");
             }
         });
-        menu.addMenuItem("Afisarea descrierii depline a articolelor dupa ID",()->{
+        menu.addMenuItem("Afisarea descrierii depline a articolelor dupa ID", () -> {
             System.out.print("Introdu identificatorul articolului sau apasa enter:");
             String articleId = new Scanner(System.in).nextLine();
-            try{
+            try {
                 Integer id = Integer.parseInt(articleId);
                 List<String> listArticle = parser.printArticleFullDescriptionById(id);
-                if(id >= 0 && id < listArticle.size()){
+                if (id >= 0 && id < listArticle.size()) {
                     System.out.println("Descrierea deplina a articolului cu numarul " + id + " este:\n" + listArticle.get(id));
-                }else{
+                } else {
                     System.out.println("Nu exista astfel de articol!");
                 }
-            }catch(NumberFormatException e){
+            } catch (NumberFormatException e) {
                 System.out.println("Nu ati introdus un numar corect!");
             }
         });
-        menu.addMenuItem("Cautare si afisare a articolelor dupa cuvintele cheie",()->{
+        menu.addMenuItem("Cautare si afisare a articolelor dupa cuvintele cheie", () -> {
             System.out.print("Introdu cuvintul cheie pentru cautare:");
             String key = new Scanner(System.in).nextLine();
             List<String> listArticle = parser.searchByKeywords(key);
-            if(key.isBlank() || listArticle.size()==0){
+            if (key.isBlank() || listArticle.size() == 0) {
                 System.out.println("Nu ati introdus nimic.");
-            }
-            else{
-                System.out.println("Lista articolelor ce contin cuvintul: " +key+ " este:\n");
-               for (String item: listArticle) {
+            } else {
+                System.out.println("Lista articolelor ce contin cuvintul: " + key + " este:\n");
+                for (String item : listArticle) {
                     System.out.println(item);
                 }
             }
         });
-        menu.addMenuItem("Cautare si afisarea articolelor dupa antet",()->{
+        menu.addMenuItem("Cautare si afisarea articolelor dupa antet", () -> {
             System.out.print("Introdu titlul pentru cautare:");
             String titleSearch = new Scanner(System.in).nextLine();
             List<Article> searchResult = parser.searchByTitle(titleSearch);
-            if(titleSearch.isBlank() || searchResult.size()==0){
+            if (titleSearch.isBlank() || searchResult.size() == 0) {
                 System.out.println("Nu ati introdus nimic/sau nu s-a gasit nimic.");
-            }
-            else{
-                System.out.println("Lista articolelor ce contin titlul: " +titleSearch+ " este:\n");
-                for (Article item: searchResult) {
+            } else {
+                System.out.println("Lista articolelor ce contin titlul: " + titleSearch + " este:\n");
+                for (Article item : searchResult) {
                     System.out.println(item.title);
                 }
             }
         });
-        menu.addMenuItem("Cautare si afisarea articolelor dupa descriere",()->{
+        menu.addMenuItem("Cautare si afisarea articolelor dupa descriere", () -> {
             System.out.print("Introdu descrierea pentru cautare:");
             String descriptionSearch = new Scanner(System.in).nextLine();
             List<Article> searchResult = parser.searchByDescription(descriptionSearch);
-            if(descriptionSearch.isBlank() || searchResult.size()==0){
+            if (descriptionSearch.isBlank() || searchResult.size() == 0) {
                 System.out.println("Nu ati introdus nimic.");
-            }
-            else{
-                System.out.println("Lista articolelor ce contin descrierea: " +descriptionSearch+ " este:\n");
-                for (Article item: searchResult) {
+            } else {
+                System.out.println("Lista articolelor ce contin descrierea: " + descriptionSearch + " este:\n");
+                for (Article item : searchResult) {
                     System.out.println(item.title);
                 }
             }
         });
-        menu.addMenuItem("Cautare si afisare a articolelor dupa autor",()->{
+        menu.addMenuItem("Cautare si afisare a articolelor dupa autor", () -> {
             System.out.print("Introdu autorul pentru cautare:");
             String creatorSearch = new Scanner(System.in).nextLine();
             List<Article> searchResult = parser.searchByCreator(creatorSearch);
-            if(creatorSearch.isBlank() || searchResult.size()==0){
+            if (creatorSearch.isBlank() || searchResult.size() == 0) {
                 System.out.println("Nu ati introdus nimic.");
-            }
-            else{
-                System.out.println("Lista articolelor ce sunt create de : " +creatorSearch+ " este:\n");
-                for (Article item: searchResult) {
+            } else {
+                System.out.println("Lista articolelor ce sunt create de : " + creatorSearch + " este:\n");
+                for (Article item : searchResult) {
                     System.out.println(item.title);
                 }
             }
         });
-        menu.addMenuItem("Afisarea numarului de cuvinte din articol",()->{
+        menu.addMenuItem("Afisarea numarului de cuvinte din articol", () -> {
             System.out.print("Introdu identificatorul articolului sau apasa enter:");
             String articleId = new Scanner(System.in).nextLine();
 
-            if(articleId.isBlank()){
+            if (articleId.isBlank()) {
                 Integer totalWordsCount = parser.getTotalWordsCount();
                 System.out.print("Numarul total de cuvinte: " + totalWordsCount);
-            }else{
-                try{
+            } else {
+                try {
                     Integer id = Integer.parseInt(articleId);
                     Integer wordsCount = parser.getWordsCountForArticle(id);
-                    System.out.print("In articolul : " +id+ " numarul de cuvinte este: " + wordsCount);
+                    System.out.print("In articolul : " + id + " numarul de cuvinte este: " + wordsCount);
 
-                }catch(NumberFormatException e){
+                } catch (NumberFormatException e) {
                     System.out.println("Nu ati introdus numar corect!");
                 }
-
             }
         });
-        menu.addMenuItem("Afișează numărul de apariții ale unui anumit cuvânt în articol",()->{
+        menu.addMenuItem("Afișează numărul de apariții ale unui anumit cuvânt în articol", () -> {
             System.out.print("Introdu cuvintul cheie pentru cautare:");
             String word = new Scanner(System.in).nextLine();
             System.out.print("Introdu identificatorul articolului in care se va cauta sau apasa enter pentru a cauta in toate:");
             String articleId = new Scanner(System.in).nextLine();
-            Integer id=0;
+            Integer id = 0;
 
-            if(word.isBlank()){
+            if (word.isBlank()) {
                 System.out.println("Nu ati introdus nimic.");
-            }
-            else{
-                if(articleId.isBlank()){
+            } else {
+                if (articleId.isBlank()) {
                     id = -1;
-                }else{
-                    try{
+                } else {
+                    try {
                         id = Integer.parseInt(articleId);
-                    }catch(NumberFormatException e){
+                    } catch (NumberFormatException e) {
                         System.out.println("Nu ati introdus un numar corect! se va cauta in toate articolele");
-                        id=0;
+                        id = 0;
                     }
                 }
             }
             Integer searchResult = parser.searchWordOccurence(word, id);
-            System.out.println("Cuvintul: " +word+ " este intilnit de:" + searchResult);
+            System.out.println("Cuvintul: " + word + " este intilnit de:" + searchResult);
         });
-        menu.addMenuItem("Iesire", ()->isRunning=false);
+        menu.addMenuItem("Iesire", () -> isRunning = false);
     }
 }
